4381e7856ca3235f636f5ab4c20d355baa21b505
SMacOne <s.macone@libero.it>
2020-06-12 12:25:30 +0200

    next commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
