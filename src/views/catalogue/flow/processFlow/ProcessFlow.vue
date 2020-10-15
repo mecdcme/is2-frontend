cb02ddab90f517c9bcf3d991278918095c8f0caf
SMacOne <s.macone@libero.it>
2020-10-15 16:07:33 +0200

    new update

--- src/views/catalogue/flow/processFlow/ProcessFlow.vue
+++ src/views/catalogue/flow/processFlow/ProcessFlow.vue
@@ -34,7 +34,12 @@
       ref="chart"
     >
     </flowchart>
-    <CModal style="width: 320px;" :show.sync="nodeDialog">
+    <CModal
+      style="width: 320px;"
+      :show.sync="nodeDialog"
+      :node.sync="nodeForm.target"
+      :operation="nodeForm.operation"
+    >
       <div class="header"><span>Edit</span></div>
       <div class="body">
         <label for="name">Name</label
@@ -54,7 +59,6 @@
           </option></select
         >
       </div>
-      <!-- <div class="footer"><button>Cancel</button><button>Ok</button></div> -->
       <template #footer>
         <CButton
           shape="square"
@@ -74,7 +78,11 @@
         </CButton>
       </template>
     </CModal>
-    <CModal style="width: 320px;" :show.sync="connectionDialog">
+    <CModal
+      style="width: 320px;"
+      :show.sync="connectionDialog"
+      :connection.sync="connectionForm.target"
+    >
       <div class="header"><span>Edit</span></div>
       <div class="body">
         <label for="name">Name</label
@@ -104,14 +112,14 @@
           Update
         </CButton>
       </template>
-      <!--  <div class="footer"><button>Cancel</button><button>Ok</button></div> -->
     </CModal>
   </div>
 </template>
 <script>
 import Vue from "vue";
 import FlowChart from "flowchart-vue";
-
+import { roundTo20 } from "@/common";
+import * as d3 from "d3";
 Vue.use(FlowChart);
 
 export default {
@@ -173,18 +181,122 @@ export default {
       this.connectionDialog = true;
     },
     nodeModalOk() {
-      this.nodeForm.target.type = this.type.value();
+      /* this.nodeForm.target.type = this.type.value(); */
       this.nodeDialog = false;
     },
     nodeModalClose() {
       this.nodeDialog = false;
     },
     connectionModalOk() {
-      this.connectionForm.target = this.type.value();
+      /*  this.connectionForm.target = this.type.value(); */
       this.connectionDialog = false;
     },
     connectionModalClose() {
       this.connectionDialog = false;
+    },
+    render: function(g, node, isSelected) {
+      node.width = node.width || 120;
+      node.height = node.height || 60;
+      let borderColor = isSelected ? "#666666" : "#bbbbbb";
+      if (node.type !== "start" && node.type !== "end") {
+        // title
+        if (node.id !== 3) {
+          g.append("rect")
+            .attr("x", node.x)
+            .attr("y", node.y)
+            .attr("stroke", borderColor)
+            .attr("class", "title")
+            .style("height", "20px")
+            .style("fill", "#f1f3f4")
+            .style("stroke-width", "1px")
+            .style("width", node.width + "px");
+          g.append("text")
+            .attr("x", node.x + 4)
+            .attr("y", node.y + 15)
+            .attr("class", "unselectable")
+            .text(() => node.name)
+            .each(function wrap() {
+              let self = d3.select(this),
+                textLength = self.node().getComputedTextLength(),
+                text = self.text();
+              while (textLength > node.width - 2 * 4 && text.length > 0) {
+                text = text.slice(0, -1);
+                self.text(text + "...");
+                textLength = self.node().getComputedTextLength();
+              }
+            });
+        }
+      }
+      // body
+      if (node.id === 3) {
+        let body = g.append("ellipse").attr("class", "body");
+        body.attr("cx", node.x + node.width / 2);
+        body.attr("cy", node.y + node.height / 2);
+        body.attr("rx", node.width / 2);
+        body.attr("ry", node.height / 2);
+        body.style("fill", "white");
+        body.style("stroke-width", "1px");
+        body.classed(node.type, true);
+        body.attr("stroke", borderColor);
+      } else {
+        let body = g.append("rect").attr("class", "body");
+        body
+          .style("width", node.width + "px")
+          .style("fill", "white")
+          .style("stroke-width", "1px");
+        if (node.type !== "start" && node.type !== "end") {
+          body.attr("x", node.x).attr("y", node.y + 20);
+          body.style("height", roundTo20(node.height - 20) + "px");
+        } else {
+          body
+            .attr("x", node.x)
+            .attr("y", node.y)
+            .classed(node.type, true)
+            .attr("rx", 30);
+          body.style("height", roundTo20(node.height) + "px");
+        }
+        body.attr("stroke", borderColor);
+      }
+
+      // body text
+      let text =
+        node.type === "start"
+          ? "Start"
+          : node.type === "end"
+          ? "End"
+          : !node.approvers || node.approvers.length === 0
+          ? "No approver"
+          : node.approvers.length > 1
+          ? `${node.approvers[0].name + "..."}`
+          : node.approvers[0].name;
+      let bodyTextY;
+      if (node.type !== "start" && node.type !== "end") {
+        if (node.id === 3) {
+          bodyTextY = node.y + 25;
+        } else {
+          bodyTextY = node.y + 25 + roundTo20(node.height - 20) / 2;
+        }
+      } else {
+        bodyTextY = node.y + 5 + roundTo20(node.height) / 2;
+      }
+      g.append("text")
+        .attr("x", node.x + node.width / 2)
+        .attr("y", bodyTextY)
+        .attr("class", "unselectable")
+        .attr("text-anchor", "middle")
+        .text(function() {
+          return text;
+        })
+        .each(function wrap() {
+          let self = d3.select(this),
+            textLength = self.node().getComputedTextLength(),
+            text = self.text();
+          while (textLength > node.width - 2 * 4 && text.length > 0) {
+            text = text.slice(0, -1);
+            self.text(text + "...");
+            textLength = self.node().getComputedTextLength();
+          }
+        });
     }
   }
 };
